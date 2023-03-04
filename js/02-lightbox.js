f1c457a37e472805290b12680a26044ab6941f76
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2023-03-04 17:15:42 +0200

    task 2 complete

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEL = document.querySelector(".gallery");
+const GalleryImagesMarkup = createGalleryImagesMarkup(galleryItems);
+
+galleryEL.insertAdjacentHTML("beforeend", GalleryImagesMarkup);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function createGalleryImagesMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`;
+    })
+    .join("");
+}
